ba41bca4042f9d91b69aa1427ed4f1d4c0a0987f
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2021-11-01 13:56:16 -0300

    commit initial

---
+++ ecommerce_rest/settings/local.py
@@ -0,0 +1,21 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+STATIC_URL = '/static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
