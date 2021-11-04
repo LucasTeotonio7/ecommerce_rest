0e996df56e77e36d909d4c64d1548c39471d42dd
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2021-11-04 07:27:30 -0300

    criado metodos PUT e DELETE da api de usuário

--- apps/users/api/api.py
+++ apps/users/api/api.py
@@ -19,3 +19,23 @@ def user_api_view(request):
             return Response(user_serializer.data)
         return Response(user_serializer.errors)
 
+@api_view(['GET', 'PUT', 'DELETE'])
+def user_detail_api_view(request, pk=None):
+
+    if request.method == 'GET':
+        user = User.objects.filter(id=pk).first()
+        user_serializer = UserSerializer(user)
+        return Response(user_serializer.data)
+
+    if request.method == 'PUT':
+        user = User.objects.filter(id=pk).first()
+        user_serializer = UserSerializer(user, data=request.data)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response(user_serializer.data)
+        return Response(user_serializer.errors)
+
+    elif request.method == 'DELETE':
+        user = User.objects.filter(id=pk).first()
+        user.delete()
+        return Response('Usuário Excluído!')
