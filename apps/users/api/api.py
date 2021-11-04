4bd0b24772dfb07b9a0af93ce13334ee9b60b514
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2021-11-03 21:41:21 -0300

    utilizado decorator @api_view

--- apps/users/api/api.py
+++ apps/users/api/api.py
@@ -1,13 +1,17 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from apps.users.models import User
 from apps.users.api.serializers import UserSerializer
 
+@api_view(['GET', 'POST'])
+def user_api_view(request):
 
-class UserAPIView(APIView):
-
-    def get(self, request):
+    if request.method == 'GET':
         users = User.objects.all()
         users_serializer = UserSerializer(users, many=True)
         return Response(users_serializer.data)
+
+    elif request.method == 'POST':
+        print(request.data)
 
--- apps/users/api/urls.py
+++ apps/users/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from apps.users.api.api import UserAPIView
+from apps.users.api.api import user_api_view
 
 urlpatterns = [
-    path('usuario/', UserAPIView.as_view(), name='usuario_api')
+    path('usuario/', user_api_view, name='usuario_api')
 ]
