110545e77f8ec3ebf14e35fec21d246b4d775462
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2021-11-06 22:24:06 -0300

    A8-Trabalhado validações no serializer

--- apps/users/api/api.py
+++ apps/users/api/api.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from apps.users.models import User
-from apps.users.api.serializers import UserSerializer
+from apps.users.api.serializers import UserSerializer, testUserSerializer
 
 @api_view(['GET', 'POST'])
 def user_api_view(request):
@@ -14,6 +14,14 @@ def user_api_view(request):
         # queryset
         users = User.objects.all()
         users_serializer = UserSerializer(users, many=True)
+
+        test_data = {
+            'name':'develop',
+            'email':'develop@gmail.com'
+        }
+        test_user = testUserSerializer(data = test_data)
+        if test_user.is_valid():
+            print('Serializer Validado')
 
         return Response(users_serializer.data, status=status.HTTP_200_OK)
 
--- apps/users/api/serializers.py
+++ apps/users/api/serializers.py
@@ -6,3 +6,19 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+class testUserSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=200)
+    email = serializers.EmailField()
+
+    def validate_name(self, value):
+        print(value)
+        return value
+
+    def validate_email(self, value):
+        print(value)
+        return value
+
+    def validate(self, data):
+        print('validação geral')
+        return data
