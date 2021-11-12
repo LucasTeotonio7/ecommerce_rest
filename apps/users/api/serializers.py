a00c75b273e2390fc7ac8294b5d5a546c98515eb
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2021-11-11 21:35:34 -0300

    A14-encrypted password in serializer

--- apps/users/api/serializers.py
+++ apps/users/api/serializers.py
@@ -8,10 +8,29 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = '__all__'
 
+    def create(self, validated_data):
+        user = User(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        update_user = super().update(instance, validated_data)
+        update_user.set_password(validated_data['password'])
+        update_user.save()
+        return update_user
+
+
+class UserListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+
     def to_representation(self, instance):
         return {
             'id': instance['id'],
-            'nome_usuario': instance['username'],
+            'name': instance['name'],
+            'username': instance['username'],
             'email': instance['email'],
-            'senha': instance['password'],
+            'password': instance['password'],
         }
