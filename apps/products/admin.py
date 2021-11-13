e0675b8c1ef2d0ec25c4ddf3b3983eac639c3fea
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2021-11-13 15:13:13 -0300

    A15-registrado models no admin do django

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from apps.products.models import *
 
 # Register your models here.
+admin.site.register(MeasureUnit)
+admin.site.register(CategoryProduct)
+admin.site.register(Indicator)
+admin.site.register(Product)
