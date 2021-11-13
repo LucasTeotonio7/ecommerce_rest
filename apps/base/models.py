e2c782ed9684d01709633d7c61176e8b82939a2c
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2021-11-13 15:10:33 -0300

    A15-Criado modelo Base

--- apps/base/models.py
+++ apps/base/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class BaseModel(models.Model):
+
+    #TODO: Define fields here
+    id = models.AutoField(primary_key=True)
+    state = models.BooleanField('Situação', default=True)
+    created_date = models.DateField('Data de criação', auto_now=False, auto_now_add=True)
+    modified_date = models.DateField('Data de modificação', auto_now=True, auto_now_add=False)
+    deleted_date = models.DateField('Data de exclusão', auto_now=True, auto_now_add=False)
+
+    class Meta:
+        abstract = True
+        verbose_name = 'Modelo Base'
+        verbose_name_plural = 'Modelos Base'
