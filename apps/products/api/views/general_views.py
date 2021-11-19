6bbd3e5180631b3d6398fc6fb4360f2ddd1ba641
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2021-11-18 23:59:22 -0300

    A18-created generic ListView

--- apps/products/api/views/general_views.py
+++ apps/products/api/views/general_views.py
@@ -1,26 +1,16 @@
-from rest_framework import generics
-
-from apps.products.models import MeasureUnit, CategoryProduct, Indicator
+from apps.base.api import GeneralListApiView
 from apps.products.api.serializers.general_serializers \
     import (MeasureUnitSerializer, CategoryProductSerializer,
             IndicatorSerializer)
 
-class MeasureUnitListAPIView(generics.ListAPIView):
+
+class MeasureUnitListAPIView(GeneralListApiView):
     serializer_class = MeasureUnitSerializer
 
-    def get_queryset(self):
-        return MeasureUnit.objects.filter(state = True)
 
-
-class CategoryProductListAPIView(generics.ListAPIView):
+class CategoryProductListAPIView(GeneralListApiView):
     serializer_class = CategoryProductSerializer
 
-    def get_queryset(self):
-        return CategoryProduct.objects.filter(state = True)
 
-
-class IndicatorListAPIView(generics.ListAPIView):
+class IndicatorListAPIView(GeneralListApiView):
     serializer_class = IndicatorSerializer
-
-    def get_queryset(self):
-        return Indicator.objects.filter(state = True)
