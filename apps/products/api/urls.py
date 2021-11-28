121e069562962480eedce553a7e1542ecea27cdd
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2021-11-28 14:06:15 -0300

    A24-RetrieveUpdateDestroyApiView

--- apps/products/api/urls.py
+++ apps/products/api/urls.py
@@ -4,8 +4,8 @@ from apps.products.api.views.general_views \
     import (MeasureUnitListAPIView, CategoryProductListAPIView,
             IndicatorListAPIView)
 from apps.products.api.views.product_views \
-    import ProductListCreateAPIView, ProductRetrieveAPIView, \
-        ProductDestroyAPIView, ProductUpdateAPIView
+    import ProductListCreateAPIView, ProductRetrieveUpdateDestroyAPIView
+
 
 
 urlpatterns = [
@@ -13,7 +13,5 @@ urlpatterns = [
     path('category_product/', CategoryProductListAPIView.as_view(), name='category_product'),
     path('indicator/', IndicatorListAPIView.as_view(), name='indicator'),
     path('product/', ProductListCreateAPIView.as_view(), name='product'),
-    path('product/retrieve/<int:pk>', ProductRetrieveAPIView.as_view(), name='product_retrieve'),
-    path('product/destroy/<int:pk>', ProductDestroyAPIView.as_view(), name='product_destroy'),
-    path('product/update/<int:pk>', ProductUpdateAPIView.as_view(), name='product_update'),
+    path('product/retrieve-update-destroy/<int:pk>', ProductRetrieveUpdateDestroyAPIView.as_view(), name='product_retrieve_update_destroy'),
 ]
--- apps/products/api/views/product_views.py
+++ apps/products/api/views/product_views.py
@@ -12,42 +12,22 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
 
     def post(self, request):
         serializer = self.serializer_class(data = request.data)
-        print(serializer)
+
         if serializer.is_valid():
-            print("entrou aqui no is valid")
             serializer.save()
             return Response({'message': 'Produto criado corretamente!'},
                 status=status.HTTP_201_CREATED)
-        print("Não entrou no is valid")
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class ProductRetrieveAPIView(generics.RetrieveAPIView):
+class ProductRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ProductSerializer
 
-    def get_queryset(self):
-        return self.get_serializer().Meta.model.objects.filter(state = True)
-
-
-class ProductDestroyAPIView(generics.DestroyAPIView):
-    serializer_class = ProductSerializer
-
-    def get_queryset(self):
-        return self.get_serializer().Meta.model.objects.filter(state = True)
-
-    def delete(self, request, pk=None):
-        product = self.get_queryset().filter(id=pk).first()
-        if product:
-            product.state = False
-            product.save()
-            return Response({"message": "Produto excluido corretamente"}, status=status.HTTP_200_OK)
-        return Response({"error": "Produto não encontrado!"}, status=status.HTTP_400_BAD_REQUEST)
-
-class ProductUpdateAPIView(generics.UpdateAPIView):
-    serializer_class = ProductSerializer
-
-    def get_queryset(self, pk):
-        return self.get_serializer().Meta.model.objects.filter(id = pk, state = True).first()
+    def get_queryset(self, pk=None):
+        if pk is None:
+            return self.get_serializer().Meta.model.objects.filter(state = True)
+        else:
+            return self.get_serializer().Meta.model.objects.filter(id=pk, state = True).first()
 
     def patch(self, request, pk=None):
         if self.get_queryset(pk):
@@ -64,3 +44,11 @@ class ProductUpdateAPIView(generics.UpdateAPIView):
                 product_serializer.save()
                 return Response(product_serializer.data, status = status.HTTP_200_OK)
             return Response(product_serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk=None):
+        product = self.get_queryset().filter(id=pk).first()
+        if product:
+            product.state = False
+            product.save()
+            return Response({"message": "Produto excluido corretamente"}, status=status.HTTP_200_OK)
+        return Response({"error": "Produto não encontrado!"}, status=status.HTTP_400_BAD_REQUEST)
