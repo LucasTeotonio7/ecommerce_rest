485a2fc4252f9efcecd57f9078173e7dd50cf8aa
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2021-11-28 13:30:46 -0300

    A23-ListCreateApiView and get queryset

--- apps/products/api/urls.py
+++ apps/products/api/urls.py
@@ -4,7 +4,7 @@ from apps.products.api.views.general_views \
     import (MeasureUnitListAPIView, CategoryProductListAPIView,
             IndicatorListAPIView)
 from apps.products.api.views.product_views \
-    import ProductListAPIView, ProductCreateAPIView, ProductRetrieveAPIView, \
+    import ProductListCreateAPIView, ProductRetrieveAPIView, \
         ProductDestroyAPIView, ProductUpdateAPIView
 
 
@@ -12,8 +12,7 @@ urlpatterns = [
     path('measure_unit/', MeasureUnitListAPIView.as_view(), name='measure_unit'),
     path('category_product/', CategoryProductListAPIView.as_view(), name='category_product'),
     path('indicator/', IndicatorListAPIView.as_view(), name='indicator'),
-    path('product/list', ProductListAPIView.as_view(), name='product_list'),
-    path('product/create', ProductCreateAPIView.as_view(), name='product_create'),
+    path('product/', ProductListCreateAPIView.as_view(), name='product'),
     path('product/retrieve/<int:pk>', ProductRetrieveAPIView.as_view(), name='product_retrieve'),
     path('product/destroy/<int:pk>', ProductDestroyAPIView.as_view(), name='product_destroy'),
     path('product/update/<int:pk>', ProductUpdateAPIView.as_view(), name='product_update'),
--- apps/products/api/views/product_views.py
+++ apps/products/api/views/product_views.py
@@ -5,12 +5,10 @@ from rest_framework.response import Response
 from apps.base.api import GeneralListApiView
 from apps.products.api.serializers.product_serializers import ProductSerializer
 
-class ProductListAPIView(GeneralListApiView):
-    serializer_class = ProductSerializer
 
-
-class ProductCreateAPIView(generics.CreateAPIView):
+class ProductListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = ProductSerializer
+    queryset = ProductSerializer.Meta.model.objects.filter(state=True)
 
     def post(self, request):
         serializer = self.serializer_class(data = request.data)
