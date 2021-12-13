4e1c3ee190ed90ccf2a5be781b3fb67b36ec11bf
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2021-12-12 21:27:23 -0300

    A26-created product routers

--- apps/products/api/routers.py
+++ apps/products/api/routers.py
@@ -1,9 +1,12 @@
-from rest_framework import urlpatterns
 from rest_framework.routers import DefaultRouter
 from apps.products.api.views.product_viewsets import ProductViewSet
+from apps.products.api.views.general_views import *
 
 router = DefaultRouter()
 
 router.register(r'products',ProductViewSet, basename='Products')
+router.register(r'measure-unit',MeasureUnitListAPIView, basename='measure_unit')
+router.register(r'indicators',CategoryProductListAPIView, basename='indicators')
+router.register(r'category-products',IndicatorListAPIView, basename='category_products')
 
 urlpatterns = router.urls
