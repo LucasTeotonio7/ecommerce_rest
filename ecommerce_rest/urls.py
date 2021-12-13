089373faf9d7cd1c7ee383dfd8fbb433f4f4c3f8
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2021-12-12 21:31:53 -0300

    A26-added 'swagger' documentation routes

--- ecommerce_rest/urls.py
+++ ecommerce_rest/urls.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Documentação da API",
+      default_version='v0.1',
+      description="Documentação pública da API ecommerce",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="ecommerce@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    re_path('swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('usuario/', include('apps.users.api.urls')),
     path('products/', include('apps.products.api.routers'))
