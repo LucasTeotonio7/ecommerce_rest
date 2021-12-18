fe5ddf765352b63d10baaa5809d6459870e1d733
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2021-12-18 16:36:46 -0300

    A29-Logout, token and sessions

--- apps/users/views.py
+++ apps/users/views.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from django.contrib.sessions.models import Session
 
+from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
@@ -28,24 +29,24 @@ class Login(ObtainAuthToken):
                         'message':'inicio de sessão'
                     }, status = status.HTTP_201_CREATED)
                 else:
-                    all_sessions = Session.objects.filter(expire_date__gte=datetime.now())
-                    if all_sessions.exists():
-                        for session in all_sessions:
-                            session_data = session.get_decoded()
-                            if(user.id == int(session_data.get('_auth_user_id'))):
-                                session.delete()
-                    token.delete()
-                    token = Token.objects.create(user = user)
-                    return Response({
-                        'token': token.key,
-                        'user': user_serializer.data,
-                        'message':'inicio de sessão'
-                    }, status = status.HTTP_200_OK)
-                    # TODO: no acess
+                    # all_sessions = Session.objects.filter(expire_date__gte=datetime.now())
+                    # if all_sessions.exists():
+                    #     for session in all_sessions:
+                    #         session_data = session.get_decoded()
+                    #         if(user.id == int(session_data.get('_auth_user_id'))):
+                    #             session.delete()
                     # token.delete()
-                    # return Response(
-                    # {'error':'este usuário já possui uma sessão ativa'},
-                    # status = status.HTTP_409_CONFLICT)
+                    # token = Token.objects.create(user = user)
+                    # return Response({
+                    #     'token': token.key,
+                    #     'user': user_serializer.data,
+                    #     'message':'inicio de sessão'
+                    # }, status = status.HTTP_200_OK)
+
+                    token.delete()
+                    return Response(
+                    {'error':'este usuário já possui uma sessão ativa'},
+                    status = status.HTTP_409_CONFLICT)
             else:
                 return Response(
                     {'error':'este usuário não pode iniciar a sessão'},
@@ -55,3 +56,39 @@ class Login(ObtainAuthToken):
             {'message':'nome do usuário ou senha incorreto'},
             status = status.HTTP_400_BAD_REQUEST)
 
+
+class Logout(APIView):
+
+    def get(self, request, *args, **kwargs):
+        try:
+            token = request.GET.get('token')
+            token = Token.objects.filter(key = token).first()
+
+            if token:
+                user = token.user
+
+                all_sessions = Session.objects.filter(expire_date__gte=datetime.now())
+                if all_sessions.exists():
+                    for session in all_sessions:
+                        session_data = session.get_decoded()
+                        if(user.id == int(session_data.get('_auth_user_id'))):
+                            session.delete()
+
+                token.delete()
+
+                session_message = 'sessões encerradas.'
+                token_message = 'token excluído.'
+
+                return Response({
+                    'token_message': token_message,
+                    'session_message': session_message
+                }, status = status.HTTP_200_OK)
+
+            return Response({
+                'error':'Não encontrado um usuário com essas credenciais'
+            }, status = status.HTTP_400_BAD_REQUEST)
+
+        except:
+            return Response({
+                'error': 'Não foi encontrado um token na requisição'
+            }, status= status.HTTP_409_CONFLICT)
--- ecommerce_rest/urls.py
+++ ecommerce_rest/urls.py
@@ -5,7 +5,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-from apps.users.views import Login
+from apps.users.views import Login, Logout
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -26,6 +26,7 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('', Login.as_view(), name="login"),
+    path('logout/', Logout.as_view(), name="logout"),
     path('usuario/', include('apps.users.api.urls')),
     path('products/', include('apps.products.api.routers'))
 ]
