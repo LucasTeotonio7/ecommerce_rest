3ea4596745ea5aaeb880e3efc88ee55e7f26f2da
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-04-26 21:36:33 -0300

    A33-connect drf + frontend

--- ecommerce_rest/urls.py
+++ ecommerce_rest/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
-    path('', Login.as_view(), name="login"),
+    path('login/', Login.as_view(), name="login"),
     path('logout/', Logout.as_view(), name="logout"),
     path('refresh-token/', UserToken.as_view(), name="refresh_token"),
     path('usuario/', include('apps.users.api.urls')),
